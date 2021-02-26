ad62f2c03cfec95d1cd122b93daec60e21bc24cb
Kevin Hertfelder <kevin.game@web.de>
2021-02-26 05:39:17 +0100

    test player controls.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {AuthenticationService} from '../../services/authentication.service';
@@ -13,9 +13,7 @@ import {RxStompService} from '@stomp/ng2-stompjs';
 })
 export class HomeComponent implements AfterViewInit {
   $player: HTMLAudioElement;
-  songId: string;
   sessionId: string;
-
   sessions: Session[] = [];
 
   @ViewChild('audioPlayer') set playerRef(ref: ElementRef<HTMLAudioElement>) {
@@ -44,15 +42,8 @@ export class HomeComponent implements AfterViewInit {
     this.sessionId = id;
     console.log('SESSION ID IS: ' + this.sessionId);
     this.rxStompService.watch(`/topic/sessions/${this.sessionId}`).subscribe((message: any) => {
-      console.log(message);
+      console.log(message.body);
     });
-  }
-  startSession(): void {
-    if (this.sessionId) {
-      this.rxStompService.publish({destination: `/app/sessions/${this.sessionId}/commands/play`, body: 'world'});
-    } else {
-      alert('No active Session!');
-    }
   }
 
   createSession(): void {
@@ -63,15 +54,9 @@ export class HomeComponent implements AfterViewInit {
       });
   }
 
-  browseSongs(): void {
-    const options =  {headers: this.authenticationService.getAuthHeaderForCurrentUser()};
-    this.http.get(`http://${environment.dbServer}/songs/all`, options)
-      .subscribe(value => console.log(value));
-  }
-
-  loadSong(): void {
+  loadSong(songId): void {
     const options = {headers: this.authenticationService.getAuthHeaderForCurrentUser(), responseType: 'text' as 'text' };
-    this.http.get(`http://${environment.dbServer}/songs/${this.songId}/data?X-NPE-PSU-Duration=PT1H`, options).subscribe(url => {
+    this.http.get(`http://${environment.dbServer}/songs/${songId}/data?X-NPE-PSU-Duration=PT1H`, options).subscribe(url => {
       const startSchedule = (new Date()).getMilliseconds() + 1000;
 
       console.log(url);
@@ -84,5 +69,45 @@ export class HomeComponent implements AfterViewInit {
         ).catch(reason => console.error(reason));
       }, startSchedule - (new Date()).getMilliseconds());
     });
+  }
+
+  startSong(): void {
+    if (this.sessionId) {
+      this.rxStompService.publish({destination: `/app/sessions/${this.sessionId}/commands/start`, body: 'text'});
+    } else {
+      alert('No active Session!');
+    }
+  }
+
+  pauseSong(): void {
+    if (this.sessionId) {
+      this.rxStompService.publish({destination: `/app/sessions/${this.sessionId}/commands/pause`, body: 'text'});
+    } else {
+      alert('No active Session!');
+    }
+  }
+
+  resumeSong(): void {
+    if (this.sessionId) {
+      this.rxStompService.publish({destination: `/app/sessions/${this.sessionId}/commands/resume`, body: 'text'});
+    } else {
+      alert('No active Session!');
+    }
+  }
+
+  stopSong(): void {
+    if (this.sessionId) {
+      this.rxStompService.publish({destination: `/app/sessions/${this.sessionId}/commands/stop`, body: 'text'});
+    } else {
+      alert('No active Session!');
+    }
+  }
+
+  skipSong(): void {
+    if (this.sessionId) {
+      this.rxStompService.publish({destination: `/app/sessions/${this.sessionId}/commands/skip`, body: 'text'});
+    } else {
+      alert('No active Session!');
+    }
   }
 }
