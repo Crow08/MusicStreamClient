a26bc9e069a63d4e05babb0ab1dc03c77c3cf734
Kevin Hertfelder <kevin.game@web.de>
2021-03-09 01:54:16 +0100

    Moved all material includes in separate module file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,26 +12,18 @@ import {LoaderComponent} from './components/loader/loader.component';
 import {InjectableRxStompConfig, RxStompService, rxStompServiceFactory} from '@stomp/ng2-stompjs';
 import {myRxStompConfig} from './my-rx-stomp.config';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
 import {SessionBrowserComponent} from './components/sessions/browser/session-browser.component';
 import {SessionCreatorComponent} from './components/sessions/session-creator/session-creator.component';
-import {MatOptionModule} from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {LatencyComponent} from './components/latency/latency.component';
 import {PlayerComponent} from './components/player/player.component';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {ImportComponent} from './components/import/import.component';
 import {OverlayContainer} from '@angular/cdk/overlay';
 import {ThemeAwareOverlayContainer} from './ThemeAwareOverlayContainer';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {ServerResultSuccessSnackBarComponent} from './components/messages/server-result-success-snack-bar.component';
+import {ServerResultErrorSnackBarComponent} from './components/messages/server-result-error-snack-bar.component';
+import {MaterialModule} from './material.module';
+import {NewObjectDialogComponent} from './components/dialog/new-object-dialog/new-object-dialog.component';
+
 
 @NgModule({
   declarations: [
@@ -44,7 +36,10 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     SessionCreatorComponent,
     LatencyComponent,
     PlayerComponent,
-    ImportComponent
+    ImportComponent,
+    ServerResultSuccessSnackBarComponent,
+    ServerResultErrorSnackBarComponent,
+    NewObjectDialogComponent
   ],
   imports: [
     FormsModule,
@@ -58,19 +53,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
       {path: 'sessions/:sessionId/lobby', component: PlayerComponent, canActivate: [AuthGuard]},
       {path: '**', redirectTo: ''}], {relativeLinkResolution: 'legacy'}),
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatListModule,
-    MatOptionModule,
-    MatSelectModule,
-    MatAutocompleteModule,
-    MatProgressBarModule,
-    MatButtonToggleModule,
-    MatSnackBarModule
+    MaterialModule
   ],
   providers: [
     {
---
+++ src/app/material.module.ts
@@ -0,0 +1,57 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatOptionModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatButtonToggleModule,
+    MatSnackBarModule,
+    MatDialogModule
+  ],
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatButtonToggleModule,
+    MatSnackBarModule,
+    MatDialogModule
+  ]
+})
+export class MaterialModule {
+}
