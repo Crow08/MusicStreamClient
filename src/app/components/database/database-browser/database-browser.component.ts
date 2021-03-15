5727be04ff6d8b0e26158039a81ce30dccbc1050
Hawky <tobiku92@gmx.de>
2021-03-16 00:18:46 +0100

    more database browse (now looks kinda good)
    - added pagination
    - added loading spinner
    - table hidden until search
    - some useless css stuff (need to change that later anyway)

--- src/app/components/database/database-browser/database-browser.component.ts
+++ src/app/components/database/database-browser/database-browser.component.ts
@@ -1,18 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
 import {Song} from 'src/app/models/song';
 import {HttpHelperService} from '../../../services/http-helper.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-database-browser',
   templateUrl: './database-browser.component.html',
   styleUrls: ['./database-browser.component.scss']
 })
-export class DatabaseBrowserComponent implements OnInit {
+export class DatabaseBrowserComponent implements OnInit, AfterViewInit {
 
-  dataSource;
-  displayedColumns: string[] = ['path', 'title', 'album_id', 'artist_id'];
+  dataSource = new MatTableDataSource<Song>();
+  displayedColumns: string[] = ['title', 'album', 'artist'];
   public modeselect = 'song';
+  loading: boolean = false;
 
   constructor(private formBuilder: FormBuilder,
               private httpHelperService: HttpHelperService) { }
@@ -20,21 +23,33 @@ export class DatabaseBrowserComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   searchQuery: FormGroup = this.formBuilder.group({
     searchObject: [, { updateOn: "change" }],
     searchKeyword: [, { updateOn: "change" }],
     searchTerm: [, {  updateOn: "change" }],
     });
+
+    
 //artist needs to be imnplemented (autocomplete)
   submitSearch(): void {
-    console.log(this.searchQuery.value["searchKeyword"]);
+    this.loading = true;
     switch (this.searchQuery.value["searchObject"]) {
       case "song":
         this.httpHelperService.getArray(`/songs/getSongsByKeyword/${this.searchQuery.value["searchKeyword"]}`, Song)
         .then((songs) => {
-          this.dataSource = songs;
+          this.dataSource.data = songs;
+          this.loading = false;
         })
-        .catch(console.error);
+        .catch((error) => {
+          console.error(error);
+          this.loading = false;
+        });
           break;
       case "artist":
         console.log("Just an artist");
