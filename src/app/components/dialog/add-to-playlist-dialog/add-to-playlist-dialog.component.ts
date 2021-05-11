5ffafa5140decb7799bb4e37917da6d74e0283a0
Hawky <tobiku92@gmx.de>
2021-05-11 18:28:38 +0200

    stopping that madness, it´s just guessing at this point

--- src/app/components/dialog/add-to-playlist-dialog/add-to-playlist-dialog.component.ts
+++ src/app/components/dialog/add-to-playlist-dialog/add-to-playlist-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GenericDataObject } from 'src/app/models/genericDataObject';
+import { ObjectSelectInputData } from '../../util/object-select/object-select.component';
 
 @Component({
   selector: 'app-add-to-playlist-dialog',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-to-playlist-dialog.component.scss']
 })
 export class AddToPlaylistDialogComponent implements OnInit {
+
+  @Input() selectedOptions: GenericDataObject[];
+  selectObjectData: ObjectSelectInputData;
 
   constructor() { }
 
